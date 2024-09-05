3d340ceb3dbba1e37154b24c7c13748692ce539c
Cowed95 <marincamilo505@gmail.com>
2024-09-05 10:36:25 -0500

    Test Nombre y Edad (Samuel Grimaldos

--- src/main/test/com/miempresa/miapp/ServicioEstudianteTest.java
+++ src/main/test/com/miempresa/miapp/ServicioEstudianteTest.java
@@ -2,13 +2,30 @@ package main.test.com.miempresa.miapp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import main.java.com.miempresa.miapp.Estudiante;
 
 class ServicioEstudianteTest {
 
+	Estudiante estudiante;
+	
+	@BeforeEach
+	void antes() {
+		estudiante = new Estudiante("Juan", 20, "asd123");
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testNombre() {
+	String nombre = estudiante.getNombre();
+	assertEquals("Juan", nombre);
+	}
+	
+	@Test
+	void testEdad() {
+		int edad = estudiante.getEdad();
+		assertEquals(20, edad);
 	}
 
 }
