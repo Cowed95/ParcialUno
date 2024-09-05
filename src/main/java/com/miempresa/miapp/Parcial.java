8b418d682c1bce2bcd6991e9f6750662d43893f2
Cowed95 <marincamilo505@gmail.com>
2024-09-05 10:18:26 -0500

    Inicio Clases (Camilo)

--- src/main/java/com/miempresa/miapp/Parcial.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.miempresa.miapp;
-
-public class Parcial {
-
-}
---
+++ src/main/java/com/miempresa/miapp/ServicioEstudiante.java
@@ -0,0 +1,32 @@
+package main.java.com.miempresa.miapp;
+
+public class ServicioEstudiante {
+
+	private EstudiantesRegistrados estudiantesRegistrados;
+	private int maximoNumeroEstudiantes;
+
+	public ServicioEstudiante(EstudiantesRegistrados estudiantesRegistrados) {
+		this.estudiantesRegistrados = estudiantesRegistrados;
+		this.maximoNumeroEstudiantes = 2000;
+	}
+
+	public boolean esEstudianteRegistrado(String matricula) {
+		if (matricula == null || matricula.isEmpty()) {
+			throw new IllegalArgumentException("No se admiten matrículas nulas o vacías");
+		}
+		return estudiantesRegistrados.buscarPorMatricula(matricula) != null;
+	}
+
+	public boolean actualizarEstudiante(String matricula, Estudiante nuevoEstudiante) {
+		return estudiantesRegistrados.actualizarEstudiante(matricula, nuevoEstudiante);
+	}
+
+	public double obtenerPromedioEdadEstudiantes() {
+		return estudiantesRegistrados.calcularPromedioEdad();
+	}
+
+	public int obtenerMaximoNumeroEstudiantes() {
+		return this.maximoNumeroEstudiantes;
+	}
+
+}
